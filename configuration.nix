5513859b01ea8b4b7bf756d6c807eb742ad5e9a3
System administrator <mdontsov2010@gmail.com>
2025-10-25 20:53:38 +0400

    Add nvidia-340-updated package.

--- configuration.nix
+++ configuration.nix
@@ -75,7 +75,7 @@
       echo "Восстановление из гибернации..."
     '';
   };
-  networking.hostName = "nixos-MSI"; # Define your hostname.
+  networking.hostName = "nixos-MSI1"; # Define your hostname.
   # Pick only one of the below networking options.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
